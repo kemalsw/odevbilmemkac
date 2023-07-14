709fbefd31a1e53644f05aca9b1aa76a584ab791
Ali Kemal Gündüz <kemall.gunduz@icloud.com>
2023-07-14 15:40:22 +0300

    firs commit

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if (time>18 && time<22)
+            {
+                Console.WriteLine("iyi akşamlar");
+            }
+            else if (time<18 && time>12)
+            {
+                Console.WriteLine("İyi günler");
+            }
+            else if (time<12 && time>5)
+            {
+                Console.WriteLine("Günaydın");
+            }
+            else
+            {
+                Console.WriteLine("İyi geceler");
+            }
+            Console.ReadLine();
+        }
+            
+    }
+}
